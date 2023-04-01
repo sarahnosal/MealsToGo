ea8fbbcb294126eaffde309d3373d3dd1c3110ef
Sarah Nosal <nosalsm@gmail.com>
2023-04-01 12:54:06 -0500

    Added connectiong from json to app

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -22,20 +22,22 @@ const RestaurantListContainer = styled(FlatList).attrs({
 `;
 
 export const RestaurantsScreen = () => {
-  const restaurantContext = useContext(RestaurantsContext);
-  console.log(restaurantContext);
+  const { restaurants, isLoading, error } = useContext(RestaurantsContext);
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <RestaurantListContainer
-        data={restaurantContext.restaurants}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
+        data={restaurants}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
         keyExtractor={(item) => item.name}
       />
     </SafeArea>
