392e8668197cd21188dcf90f7a510e891963b060
Sarah Nosal <nosalsm@gmail.com>
2023-06-28 19:20:35 -0500

    Added a nice settings page

---
+++ src/infrastructure/navigation/settings.navigator.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+
+const SettingsStack = createStackNavigator();
+
+export const SettingsNavigator = ({ route, navigation }) => {
+  return (
+    <SettingsStack.Navigator
+      headerMode="screen"
+      screenOptions={{
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      <SettingsStack.Screen
+        options={{
+          header: () => null,
+        }}
+        name="Settings"
+        component={SettingsScreen}
+      />
+      <SettingsStack.Screen name="Favorites" component={() => null} />
+    </SettingsStack.Navigator>
+  );
+};
